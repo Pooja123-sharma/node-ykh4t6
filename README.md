8a31a1c8449e33da6608d4a39993bfdcab91d8b4
Pooja123-sharma <117913332+Pooja123-sharma@users.noreply.github.com>
2022-12-24 18:52:44 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-ykh4t6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-ykh4t6)
---
+++ index.js
@@ -0,0 +1,21 @@
+// check if input is number or string 
+let  a=100 ;
+let b='number';
+let c='string';
+if ( type of a=== type of number (a)){
+  console.log (b);
+}else if ( type of a === type of a.to string()){
+  console.log(c);
+}else {
+  console.log(a);
+}
+
+//check if input is even or odd
+let d=2002;
+let e='odd';
+if (d % 2== 0){
+  console.log (e);
+}else {
+  console.log(f);
+}
+
